418d67addaf64c1431184dc0aa0b46f7f0b2ef0c
ibrah2278 <nouhouibrahim227@gmail.com>
2025-07-05 14:48:48 +0200

    gestion probleme d'affichage image

--- app/routes/media_routes.py
+++ app/routes/media_routes.py
@@ -1,25 +1,9 @@
-from flask import Blueprint, send_from_directory, jsonify, url_for, current_app
+from flask import Blueprint, send_from_directory
 import os
 
 media_routes = Blueprint('media_routes', __name__)
 
-@media_routes.route('/static/uploads/<filename>')
-def get_image(filename):
-    return send_from_directory('static/uploads', filename)
-
-@media_routes.route('/api/images', methods=['GET'])
-def get_images():
-    image_folder = os.path.join(current_app.root_path, 'static', 'images')
-    images = []
-    for filename in os.listdir(image_folder):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-            # Construire l'URL complète
-            url = url_for('static', filename=f'images/{filename}', _external=True)
-            images.append(url)
-    return jsonify(images)
-
-from flask import send_from_directory
-
-@media_routes.route('/static/uploads/images/<filename>')
+@media_routes.route('/media/<filename>')
 def serve_uploaded_image(filename):
-    return send_from_directory('static/uploads/images', filename)
+    path = os.path.join('static', 'uploads', 'images')
+    return send_from_directory(path, filename)
