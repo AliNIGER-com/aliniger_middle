2e38af86534dd45bf96880752cd8029fc2115762
ibrah2278 <nouhouibrahim227@gmail.com>
2025-07-18 10:06:44 +0200

    resolution problem base de donnee

--- app/config.py
+++ app/config.py
@@ -2,7 +2,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/aliniger_db'
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql+mysqlconnector://root:TkAdlBaXUclWYbIPUhTprVWwXBwQFqIa'
+        '@crossover.proxy.rlwy.net:43313/railway'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'une_chaine_complexe_et_secrete'
     JWT_TOKEN_LOCATION = ["headers"]
